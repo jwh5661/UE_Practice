a016d0800ee3d500d36c47d226637cf85ba99739
jwh5661 <jwh5661@gmail.com>
2026-01-16 15:10:33 +0900

    2026/01/16
    Practice UE

---
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	int MyValue;
+	MyValue = -10 * -5;
+	UE_LOG(LogTemp, Display, TEXT("MyValue is %d"), MyValue);
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
