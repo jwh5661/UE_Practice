a5fd9f69f108fb948169bc8a961f988bca2d9b5d
jwh5661 <jwh5661@gmail.com>
2026-04-11 23:34:37 +0900

    2026/04/11

--- CommanderDefense/Source/CommanderDefense/CommanderDefense.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CommanderDefense.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CommanderDefense, "CommanderDefense" );
